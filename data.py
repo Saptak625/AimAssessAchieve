5958016aedf9a18faed1b91c82240ec718dda48a
SaptakDnth <66028584+Saptak625@users.noreply.github.com>
2023-07-17 17:22:25 -0400

    Finish Aim Assess Achieve V1

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 # Data Controller for AimAssessAccomplish
 import json
 import os
+import datetime
 
 # Check if stats file exists, if not create it
 if not os.path.isfile('stats.json'):
@@ -10,12 +11,12 @@ if not os.path.isfile('stats.json'):
 # Check if settings file exists, if not create it
 if not os.path.isfile('settings.json'):
     with open('settings.json', 'w') as settings_file:
-        json.dump({'session_time': 30, 'break_time': 10}, settings_file)
+        json.dump({'session_time': 30, 'break_time': 10, 'notification_time': 5}, settings_file)
 
 # Check if current_session file exists, if not create it
 if not os.path.isfile('current_session.json'):
     with open('current_session.json', 'w') as current_session_file:
-        json.dump({'target_goal': '', 'session_end_time': ''}, current_session_file)
+        json.dump({'target_goal': '', 'session_start_time': None, 'session_end_time': None, 'is_break': False}, current_session_file)
 
 
 # Read all files
@@ -32,9 +33,9 @@ with open('current_session.json', 'r') as current_session_file:
 def get_stats():
     return stats
 
-def recalculate_stats(new_session, new_goal_met):
+def recalculate_stats(goal_met):
     stats['sessions_completed'] = stats['sessions_completed'] + 1
-    if new_goal_met:
+    if goal_met:
         stats['goals_met'] = stats['goals_met'] + 1
         stats['current_streak'] = stats['current_streak'] + 1
         if stats['current_streak'] > stats['longest_streak']:
@@ -47,23 +48,51 @@ def recalculate_stats(new_session, new_goal_met):
 def get_settings():
     return settings
 
-def set_settings(new_session_time, new_break_time):
-    settings['session_time'] = new_session_time
-    settings['break_time'] = new_break_time
+def set_settings(new_session_time, new_break_time, new_notification_time):
+    settings['session_time'] = float(new_session_time)
+    settings['break_time'] = float(new_break_time)
+    settings['notification_time'] = float(new_notification_time)
     with open('settings.json', 'w') as settings_file:
         json.dump(settings, settings_file)
 
 def get_current_session():
     return current_session
 
-def set_current_session(new_target_goal, new_session_end_time):
+def set_current_session(new_target_goal, new_session_end_time, new_is_break):
     current_session['target_goal'] = new_target_goal
-    current_session['session_end_time'] = new_session_end_time
+    current_session['session_start_time'] = int(datetime.datetime.now().timestamp())
+    current_session['session_end_time'] = int(new_session_end_time.timestamp())
+    current_session['is_break'] = new_is_break
     with open('current_session.json', 'w') as current_session_file:
         json.dump(current_session, current_session_file)
 
+def end_current_session(goal_met, focus_level):
+    # Save session data to all_sessions folder
+    # Check that all sessions folder exists, if not create it
+    if not os.path.isdir('all_sessions'):
+        os.mkdir('all_sessions')
+
+    # Name the file so it starts with the date and time of the session
+    session_start_time = datetime.datetime.fromtimestamp(current_session['session_start_time'])
+    session_start_time_string = session_start_time.strftime('%Y-%m-%d_%H-%M-%S')
+    
+    # Create the file
+    with open('all_sessions/' + session_start_time_string + '.json', 'w') as session_file:
+        session_copy = current_session.copy()
+        session_copy['goal_met'] = goal_met
+        session_copy['focus_level'] = focus_level
+        json.dump(session_copy, session_file)
+
+    # Update stats
+    recalculate_stats(goal_met)
+
+    # Reset current session
+    reset_current_session()
+
 def reset_current_session():
     current_session['target_goal'] = ''
+    current_session['session_start_time'] = ''
     current_session['session_end_time'] = ''
+    current_session['is_break'] = False
     with open('current_session.json', 'w') as current_session_file:
         json.dump(current_session, current_session_file)
--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import tkinter as tk
 from tkinter import ttk
 import tkinter.font as tkFont
 from tkinter import messagebox
+import datetime
+from plyer import notification
+import uuid
 
-from data import get_stats, recalculate_stats, get_settings, set_settings, get_current_session, set_current_session, reset_current_session
+from data import get_stats, get_settings, set_settings, get_current_session, set_current_session, end_current_session, reset_current_session
 
 class AimAssessAccomplish(tk.Frame):
     def __init__(self, root, master=None):
@@ -34,32 +37,13 @@ class AimAssessAccomplish(tk.Frame):
         self.notebook.add(self.settings_tab, text="Settings")
 
         # Get Data from data.py
+        current_session = get_current_session()
         all_stats = get_stats()
         all_settings = get_settings()
 
         # Creating the widgets for the Session frame
-        self.name = ttk.Label(self.session_tab, text="Aim Assess Achieve", font=('Helvetica', 20, 'bold', 'underline'))
-        self.name.pack(pady=10)
-
-        self.session_label = ttk.Label(self.session_tab, text="Session:", font=('Helvetica', 16, 'bold'))
-        self.session_label.pack(pady=10)
-
-        self.session_target = ttk.Label(self.session_tab, text="Set a target goal:", font=('Helvetica', 12))
-        self.session_target.pack(pady=10)
-
-        self.session_target_entry = ttk.Entry(self.session_tab, width=150, justify=tk.CENTER)
-        self.session_target_entry.pack(pady=(0, 30))
-
-        self.session_timer = ttk.Label(self.session_tab, text="Set a timer in mins:", font=('Helvetica', 12))
-        self.session_timer.pack(pady=10)
-
-        # Make field number only
-        self.session_timer_entry = ttk.Entry(self.session_tab, width=15, justify=tk.CENTER)
-        self.session_timer_entry.insert(0, all_settings['session_time'])
-        self.session_timer_entry.pack(pady=(0, 30))
-
-        self.start_session_btn = ttk.Button(self.session_tab, text="Start Session", command=self.start_session)
-        self.start_session_btn.pack(pady=30)
+        self.met_goal = tk.IntVar()
+        self.default_session_gui()
 
         # Creating the widgets for the Stats frame
         self.name = ttk.Label(self.stats_tab, text="Aim Assess Achieve", font=('Helvetica', 20, 'bold', 'underline'))
@@ -99,7 +83,14 @@ class AimAssessAccomplish(tk.Frame):
 
         self.break_time_default_entry = ttk.Entry(self.settings_tab, width=15, justify=tk.CENTER)
         self.break_time_default_entry.insert(0, all_settings['break_time'])
-        self.break_time_default_entry.pack(pady=(0, 10))
+        self.break_time_default_entry.pack(pady=(0, 30))
+
+        self.notify_time_default = ttk.Label(self.settings_tab, text="Set the default notification update in mins:", font=('Helvetica', 12))
+        self.notify_time_default.pack(pady=10)
+
+        self.notify_time_default_entry = ttk.Entry(self.settings_tab, width=15, justify=tk.CENTER)
+        self.notify_time_default_entry.insert(0, all_settings['notification_time'])
+        self.notify_time_default_entry.pack(pady=(0, 10))
 
         self.save_settings_btn = ttk.Button(self.settings_tab, text="Save Settings", command=self.save_settings)
         self.save_settings_btn.pack(pady=(5, 30))
@@ -114,20 +105,37 @@ class AimAssessAccomplish(tk.Frame):
         self.menubar = tk.Menu(self.root)
         self.root.config(menu=self.menubar)
 
-    def start_session(self):
-        # When the start session button is clicked, then remove all the widgets from the session tab and add a clock progress bar. Show the target goal underneath. With a button to end the session early.
-        # Remove all the widgets from the session tab
-        self.target_goal = self.session_target_entry.get()
-        self.session_time = round(float(self.session_timer_entry.get()) * 60)
-        self.time_remaining = self.session_time
+        self.session_id = str(uuid.uuid4())
 
+        # If the user has a current session, then update the session tab to show the current session.
+        if current_session['session_start_time']:
+            self.target_goal = current_session['target_goal']
+            self.session_time = int(current_session['session_end_time'] - current_session['session_start_time'])
+            self.time_remaining = int(current_session['session_end_time'] - datetime.datetime.now().timestamp())
+            if self.time_remaining <= 0:
+                self.time_remaining = 0
+            if current_session['is_break']:
+                self.start_break(auto=True)
+            else:
+                self.start_session(auto=True)
+
+
+    def reset_session_gui(self):
+        # Remove all the widgets from the session tab
         for widget in self.session_tab.winfo_children():
             widget.destroy()
+
+
+    def timer_gui(self, is_break=False):
+        self.reset_session_gui()
+
+        # Generate an id
+        self.session_id = str(uuid.uuid4())
 
         self.name = ttk.Label(self.session_tab, text="Aim Assess Achieve", font=('Helvetica', 20, 'bold', 'underline'))
         self.name.pack(pady=10)
 
-        self.settings_label = ttk.Label(self.session_tab, text="Session in Progress:", font=('Helvetica', 16, 'bold'))
+        self.settings_label = ttk.Label(self.session_tab, text="Session in Progress:" if not is_break else "Taking a Break!", font=('Helvetica', 16, 'bold'))
         self.settings_label.pack(pady=10)
 
         # Time Remaining for the session
@@ -140,6 +148,8 @@ class AimAssessAccomplish(tk.Frame):
         # Add a clock progress bar
         self.clock = ttk.Progressbar(self.session_tab, orient=tk.HORIZONTAL, length=1000, mode='determinate')
         self.clock.pack(pady=(10, 30))
+        time_percent = (self.session_time - self.time_remaining) / self.session_time
+        self.clock['value'] = time_percent * 100
 
         # Show the target goal underneath
         self.session_target = ttk.Label(self.session_tab, text=f"Target Goal:", font=('Helvetica', 12, 'bold'))
@@ -149,44 +159,191 @@ class AimAssessAccomplish(tk.Frame):
         self.session_target_val.pack(pady=10)
 
         # With a button to end the session early.
-        self.end_session_btn = ttk.Button(self.session_tab, text="End Session", command=self.end_session)
+        self.end_session_btn = ttk.Button(self.session_tab, text=f"End {'Break' if is_break else 'Session'} Early", command=self.end_session_gui)
         self.end_session_btn.pack(pady=10)
 
         # Start the timer
-        self.update_clock()
+        self.update_clock(self.session_id)
 
-    def end_session(self):
+        # Start the notification timer
+        self.update_notifications(self.session_id)
+
+
+    def start_session(self, auto=False):
+        # When the start session button is clicked, then remove all the widgets from the session tab and add a clock progress bar. Show the target goal underneath. With a button to end the session early.
+        # Remove all the widgets from the session tab
+        if not auto:
+            self.target_goal = self.session_target_entry.get()
+            self.session_time = round(float(self.session_timer_entry.get()) * 60)
+            self.time_remaining = self.session_time
+
+            # Update the session
+            set_current_session(self.target_goal, datetime.datetime.now() + datetime.timedelta(seconds=self.session_time), False)
+
+        # Create the timer GUI
+        self.timer_gui()
+
+
+    def end_session_gui(self):
         # Ask the user if they met their target goal
         # Ask for their focus level
+        # Disable the end session button
+        current_session = get_current_session()
+        if current_session['is_break']:
+            self.notify("Aim Assess Achieve", "Your break has ended.")
+            self.end_session()
+        else:
+            self.notify("Aim Assess Achieve", "Your session has ended. Please assess your progress.")
+            self.end_session_btn['state'] = 'disabled'
+
+            self.end_session_window = tk.Toplevel(self.root)
+            self.end_session_window.title("End Session")
+            self.end_session_window.geometry("500x500")
+            self.name = ttk.Label(self.end_session_window, text="Aim Assess Achieve", font=('Helvetica', 20, 'bold', 'underline'))
+            self.name.pack(pady=10)
+
+            self.settings_label = ttk.Label(self.end_session_window, text="Session Ended", font=('Helvetica', 16, 'bold'))
+            self.settings_label.pack(pady=10)
+
+            self.end_session_lbl = ttk.Label(self.end_session_window, text="Did you meet your target goal?", font=('Helvetica', 16, 'bold'))
+            self.end_session_lbl.pack(pady=10)
+
+            self.end_session_goal = ttk.Label(self.end_session_window, text=f"Goal: {self.target_goal}", font=('Helvetica', 12, 'bold'))
+            self.end_session_goal.pack(pady=10)
+
+            self.check_button = ttk.Checkbutton(self.end_session_window, text="Yes", variable=self.met_goal)
+            self.check_button.pack(pady=10)
+
+            self.focus_level_lbl = ttk.Label(self.end_session_window, text="What was your focus level (1-10)?", font=('Helvetica', 16, 'bold'))
+            self.focus_level_lbl.pack(pady=10)
+
+            self.focus_level_entry = ttk.Entry(self.end_session_window, width=15, justify=tk.CENTER)
+            self.focus_level_entry.pack(pady=(10, 30))
+
+            self.continue_button = ttk.Button(self.end_session_window, text="Continue", command=self.end_session)
+            self.continue_button.pack(pady=10)
+            self.break_button = ttk.Button(self.end_session_window, text="Take a Break", command=self.start_break)
+            self.break_button.pack(pady=10)
+
+
+    def update_session(self):
         # Update the stats
-        # Ask if they want to take a break or start a new session
-        # If they want to take a break, then start a break session
-        # Else reset the session tab
-        pass
+        current_session = get_current_session()
+        if not current_session['is_break']:
+            end_current_session(bool(self.met_goal.get()), int(self.focus_level_entry.get()) if self.focus_level_entry.get() else None)
 
-    def start_break(self):
-        pass
+            # Close the end session window
+            self.end_session_window.destroy()
+        else:
+            reset_current_session()
 
-    def end_break(self):
-        pass
+        # Reset the session_id
+        self.session_id = str(uuid.uuid4())
+
+
+    def end_session(self):
+        # Add the current session to the session history and update the stats
+        self.update_session()
+
+        # Reset the session tab
+        self.reset_session_gui()
+        self.default_session_gui()
+
+
+    def default_session_gui(self):
+        all_settings = get_settings()
+        self.name = ttk.Label(self.session_tab, text="Aim Assess Achieve", font=('Helvetica', 20, 'bold', 'underline'))
+        self.name.pack(pady=10)
+
+        self.session_label = ttk.Label(self.session_tab, text="Session:", font=('Helvetica', 16, 'bold'))
+        self.session_label.pack(pady=10)
+
+        self.session_target = ttk.Label(self.session_tab, text="Set a target goal:", font=('Helvetica', 12))
+        self.session_target.pack(pady=10)
+
+        self.session_target_entry = ttk.Entry(self.session_tab, width=150, justify=tk.CENTER)
+        self.session_target_entry.pack(pady=(0, 30))
+
+        self.session_timer = ttk.Label(self.session_tab, text="Set a timer in mins:", font=('Helvetica', 12))
+        self.session_timer.pack(pady=10)
+
+        self.session_timer_entry = ttk.Entry(self.session_tab, width=15, justify=tk.CENTER)
+        self.session_timer_entry.insert(0, all_settings['session_time'])
+        self.session_timer_entry.pack(pady=(0, 5))
+
+        self.start_session_btn = ttk.Button(self.session_tab, text="Start Session", command=self.start_session)
+        self.start_session_btn.pack(pady=10)
+
+
+    def start_break(self, reset=False, auto=False):
+        # Add the current session to the session history and update the stats
+        self.update_session()
+
+        # Set the target goal and session time
+        if not auto:
+            all_settings = get_settings()
+            self.target_goal = 'Take a Break!'
+            self.session_time = round(all_settings['break_time'] * 60)
+            self.time_remaining = self.session_time
+
+            # Update the session
+            set_current_session(self.target_goal, datetime.datetime.now() + datetime.timedelta(seconds=self.session_time), True)
+
+        # Create the timer GUI
+        self.timer_gui(is_break=True)
+        
 
     def format_time_remaining(self):
         # Format the time to be in the format of mm:ss
         minutes = self.time_remaining // 60
         seconds = self.time_remaining % 60
+        if seconds < 10:
+            seconds = f"0{seconds}"
         return f"{minutes}:{seconds}"
 
-    def update_clock(self):
+
+    def update_clock(self, id):
+        # Check if the session has been ended
+        if self.session_id != id:
+            return
+
         # Update the clock progress bar
         time_percent = (self.session_time - self.time_remaining) / self.session_time
-        self.clock['value'] = time_percent * 100
-        self.time_remaining -= 1
-        self.time_remaining_val['text'] = self.format_time_remaining()
+        try:
+            self.clock['value'] = time_percent * 100
+            self.time_remaining -= 1
+            self.time_remaining_val['text'] = self.format_time_remaining()
+            if self.time_remaining > 0:
+                self.root.after(1000, self.update_clock, id)
+            else:
+                self.end_session_gui()
+        except tk.TclError:
+            pass
+
+
+    def update_notifications(self, id):
+        # Check if the session has been ended
+        if self.session_id != id:
+            return
+        
+        all_settings = get_settings()
+        msg = f"Time remaining: {self.format_time_remaining()}"
+        self.notify("Aim Assess Achieve", msg)
         if self.time_remaining > 0:
-            self.root.after(1000, self.update_clock)
+            self.root.after(int(all_settings['notification_time']*60*1000), self.update_notifications, id)
+
 
     def save_settings(self):
-        set_settings(self.session_time_default_entry.get(), self.break_time_default_entry.get())
+        set_settings(self.session_time_default_entry.get(), self.break_time_default_entry.get(), self.notify_time_default_entry.get())
+
+
+    def notify(self, title, message):
+        notification.notify(
+            title=title,
+            message=message,
+            timeout=10  # The notification will automatically close after 10 seconds
+        )
+
 
     def toggle_mode(self):
         # NOTE: The theme's real name is azure-<mode>
@@ -197,10 +354,12 @@ class AimAssessAccomplish(tk.Frame):
             # Set dark theme
             self.root.tk.call("set_theme", "dark")
 
+
 def main():
     root = tk.Tk()
     app = AimAssessAccomplish(root)
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
